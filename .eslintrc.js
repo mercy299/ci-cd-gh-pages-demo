6a9a9fed735cf128afd474d08a1ace2bfa9756e4
mercy299 <mercysam299@gmail.com>
2025-06-29 11:43:39 +0100

    enforcd single quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,10 @@
-
 module.exports = {
   env: {
     browser: true,
     es2021: true,
     jest: true,
   },
-  extends: ['react-app', 'eslint:recommended'],
+  extends: ['react-app', 'eslint:recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
