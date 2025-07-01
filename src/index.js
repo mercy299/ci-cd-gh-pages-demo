49e418a97c607f505183ac2f9da6b6282408e534
mercy299 <mercysam299@gmail.com>
2025-07-01 11:41:13 +0100

    Fix homepage URL for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component to the root element
 root.render(<App />);
